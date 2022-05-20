adedc26e031b31eb6b5a7e65aca1d27afa9900f4
Veera <sveera.2001@gmail.com>
2022-05-20 14:13:57 -0400

    Increment datapoints and implement rolling datapoints

--- examples/main/main.rs
+++ examples/main/main.rs
@@ -2,7 +2,7 @@ use emwa_rs::EMWA;
 
 fn main() {
     println!("this is a test");
-    let mut risk = EMWA::new(0.4);
+    let mut risk = EMWA::new(1 as f64, Alpha::Static);
     for i in 1..100 {
         risk.add(i as f64);
     }
--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,12 @@ pub struct EMWA {
     alpha: f64,
     value: f64,
     datapoints: u32,
+    time: f64,
+}
+
+enum Alpha {
+    Static,
+    Dynamic,
 }
 
 impl EMWA {
@@ -28,11 +34,13 @@ impl EMWA {
             alpha,
             value: 0f64,
             datapoints: 0,
+            time: 0f64,
         }
     }
 
     pub fn add(&mut self, data: f64) -> f64 {
-        if self.datapoints == 0 {
+        self.datapoints += 1;
+        if self.datapoints == 1 {
             self.value = data;
             self.value
         } else {
@@ -41,6 +49,25 @@ impl EMWA {
             self.value
         }
     }
+
+    fn compute_alpha(time: f64) -> f64 {
+        let diff = time - self.time;
+        (- diff / self.datapoints).exp()
+    }
+
+    pub fn add_with_time(&mut self, data: f64, time: f64) -> f64 {
+        self.datapoints += 1;
+        if self.datapoints == 1 {
+            self.value = data;
+            self.value
+        } else {
+            let new_alpha = self.compute_alpha(time);
+            let new_value = new_alpha * data + (1f64 - new_alpha) * self.value;
+            self.value = new_value;
+            self.value
+        }
+    }
+        
 
     pub fn value(&self) -> f64 {
         self.value
