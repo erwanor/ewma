0542c674cf53dbf73358a9ef6a677c35898429a8
erwanor <erwan.ounn.84@gmail.com>
2022-05-28 00:39:19 +0200

    tests: update test to match new api

--- examples/main/main.rs
+++ examples/main/main.rs
@@ -1,11 +1,27 @@
 use emwa_rs::*;
 
 fn main() {
-    println!("this is a test");
-    let mut risk = EMWA::new(1 as f64, Alpha::Static);
+    println!("adding datapoints to a regular timeseries:");
+    let mut risk = EMWA::new(1 as f64, Smoothing::Static);
     for i in 1..100 {
         risk.add(i as f64).unwrap();
     }
 
-    println!("value: {}", risk.value())
+    println!("value: {}", risk.value());
+
+    println!("adding datapoints to a irregular timeseries (dynamic smoothing):");
+
+    let mut observations = EMWA::new(0.5f64, Smoothing::Dynamic);
+
+    let mut clock = 100000; // some arbitrary clock
+
+    for i in 1..100 {
+        observations
+            .add_with_time(i as f64, clock as f64)
+            .expect("failed to add datapoint");
+
+        clock += 1;
+    }
+
+    println!("observations emwa: {}", observations.value());
 }
